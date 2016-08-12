f125cfbe687920040e74d6d226f472b314526963
Ignacio Andrada <ignacioandrada@MacBook-Pro-de-Ignacio.local>
2016-08-12 09:58:38 -0300

    Flow payment create

---
+++ app/controllers/PaymentFlowController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+import utils.AgencyConfigurationDto;
+import utils.TravelClubUtils;
+
+public class PaymentFlowController extends Controller {
+
+    public static void index() {
+        AgencyConfigurationDto agencyConfigurationDto = TravelClubUtils.getAgencyConfiguration("56f2d58ce4b0e66b4c0cd92e");
+
+        render(agencyConfigurationDto);
+    }
+
+}
