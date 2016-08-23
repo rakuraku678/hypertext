acaba9ef42de564e43a3dd4f3b23878d96ab0bc7
Nicolas Sandoval <nsandoval@grayshirts.com.ar>
2016-08-23 12:12:48 -0300

    Add integration with apiflights

--- app/controllers/FlightsDataController.java
+++ app/controllers/FlightsDataController.java
@@ -1,19 +1,25 @@
 package controllers;
 
+import com.google.gson.JsonElement;
 import play.mvc.Controller;
-import utils.FlightsDataDto;
-
-import java.util.concurrent.TimeUnit;
+import utils.ApiFlightsSdk.v1.FlightsSearch;
 
 public class FlightsDataController extends Controller {
 
     public static void index() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(5);
-        renderTemplate("FlightsDataController/fligthsData.html");
+        FlightsSearch flightsSearch = new FlightsSearch();
+        flightsSearch.setOrigin(params.get("origin"));
+        flightsSearch.setDestination(params.get("destination"));
+        flightsSearch.setDeparturedate(params.get("departuredate"));
+        flightsSearch.setReturndate(params.get("returndate"));
+        flightsSearch.setPassengercount(params.get("passengercount"));
+        JsonElement flightsResults = flightsSearch.process();
+
+        renderTemplate("FlightsDataController/flightsData.html",flightsResults);
     }
 
     public static void priceSuggestionMatrix() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(5);
+        //TimeUnit.SECONDS.sleep(5);
         render();
     }
 
---
+++ app/utils/ApiFlightsSdk/v1/ApiFlightsSDKBase.java
@@ -0,0 +1,47 @@
+package utils.ApiFlightsSdk.v1;
+
+import com.google.gson.JsonElement;
+import play.Play;
+import play.libs.WS;
+
+public abstract class ApiFlightsSDKBase {
+
+    private static final String CLIENT_ID = Play.configuration.getProperty("api.flight.username");
+    private static final String CLIENT_SECRET = Play.configuration.getProperty("api.flight.password");
+    private static final String ENV_URL = Play.configuration.getProperty("api.flight.url");
+
+    protected static WS.WSRequest prepareRequest(String endpoint) {
+        String url = ENV_URL + endpoint;
+        System.out.println(url);
+        WS.WSRequest request = WS.url(url);
+        request.authenticate(CLIENT_ID, CLIENT_SECRET);
+        return request;
+    }
+
+    protected static JsonElement processResponse(WS.WSRequest request) {
+        System.out.println("//****************************************//");
+        System.out.println("//               Api Flights              //");
+        System.out.println("//****************************************//");
+        System.out.println("// request_url: " + request.url );
+        System.out.println("// request_params: " + request.parameters );
+
+        try {
+            WS.HttpResponse response = request.get();
+            JsonElement jsonResponse = response.getJson();
+
+            if (!response.success()) {
+                System.out.println("// responde_code: " + response.getStatus());
+                System.out.println("// responde: " + jsonResponse);
+                System.out.println("//****************************************//");
+                throw new RuntimeException("Api Flights ERROR");
+            }
+            System.out.println("// jsonResponse: " + jsonResponse );
+            System.out.println("//****************************************//");
+
+            return jsonResponse;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Api Flights process response fail.");
+        }
+    }
+}
