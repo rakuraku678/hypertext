6bc4dbad5bade5177fe26752896f145df8fb4174
rakuraku <diegochiari86@gmail.com>
2016-11-23 19:13:50 -0300

    widget busqueda

---
+++ app/controllers/SearchController.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import play.Play;
+import play.libs.WS;
+import play.libs.WS.WSRequest;
+import play.mvc.Controller;
+
+import com.google.gson.JsonElement;
+
+import dto.ErrorDto;
+
+public class SearchController extends Controller {
+
+    public static void getAutocomplete(String q) {
+       
+    	
+    	System.out.println("me llego: "+q);
+    	
+    	
+    	try {
+	    	
+	    	String url =  Play.configuration.getProperty("api.flight.url")+"/v1/getAutocomplete?q="+q;
+	    	System.out.println("pegandole a: "+url);
+	    	WSRequest request = WS.url(url);
+	    	
+	    	//request.authenticate(paymentApiUser, paymentApiPassword);
+			
+			JsonElement response = request.get().getJson();
+			System.out.println("sending response: "+response.toString());
+			renderJSON(response);
+			
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace().toString());
+    		ErrorDto errorDto = new ErrorDto(e.getMessage());
+        	renderJSON(errorDto);
+		}
+    	
+    }
+
+}
---
+++ app/dto/ErrorDto.java
@@ -0,0 +1,10 @@
+package dto;
+
+public class ErrorDto {
+
+    public String error;
+
+    public ErrorDto(String message) {
+        this.error = message;
+    }
+}
