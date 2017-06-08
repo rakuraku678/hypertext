e763f296e14a303e3ab6f42312f1bfccb1943ff4
Victor Nuñez <vnunez@travelclub.cl>
2017-06-08 19:16:17 +0000

    por víctor nunez

--- app/views/PaymentFlowController/checkout.js
+++ app/views/PaymentFlowController/checkout.js
@@ -81,6 +81,7 @@ $(document).ready(function () {
                     regexp: {
                         regexp: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/,
                         message: 'día/mes/año'
+                    }
                 }
             },
             validateemail: {
