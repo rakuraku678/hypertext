ea44db2b3cabef7cf2f0768c6d0383b769475ba0
Diego <diego@grayshirts.com.ar>
2017-09-05 14:35:23 -0300

    se muestran los minutos de booking obtenidos de flights

--- app/controllers/PaymentFlowController.java
+++ app/controllers/PaymentFlowController.java
@@ -14,6 +14,8 @@ import com.google.gson.JsonParser;
 
 import play.mvc.Controller;
 import utils.AgencyConfigurationDto;
+import utils.ConfigurationDto;
+import utils.FlightsUtils;
 import utils.JsonUtils;
 import utils.TravelClubUtils;
 import utils.ApiFlightsSdk.v1.AirRules;
@@ -87,10 +89,9 @@ public class PaymentFlowController extends Controller {
 
     public static void processError(String type){
         PromotionDto promotionDto = new Promotion().getDefault();
-
         AgencyConfigurationDto agencyConfigurationDto = TravelClubUtils.getAgencyConfiguration(promotionDto.agency.externalId);
-
-        render(agencyConfigurationDto, type);
+        ConfigurationDto configurationDto = FlightsUtils.getConfiguration();
+        render(agencyConfigurationDto, type, configurationDto);
     }
 
     public static void javascript() {
