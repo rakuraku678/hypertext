f051b6ce895a91aa5242e00b4bffd8a55d7d8292
aspen <jmf@grayshirts.com.ar>
2017-10-19 13:02:06 -0300

    se agrego feedback de pasajero frecuente

--- app/utils/ApiFlightsSdk/v1/Alliance.java
+++ app/utils/ApiFlightsSdk/v1/Alliance.java
@@ -1,5 +1,7 @@
 package utils.ApiFlightsSdk.v1;
 
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import play.libs.WS;
@@ -7,13 +9,14 @@ import utils.dtos.AgencyDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Alliance extends ApiFlightsSDKBase {
 
     private static final String ENDPOINT = "/v1/alliance/";
 
-    public static List<String> getFFPWhiteList(String AirlineIataCode){
-        WS.WSRequest request = prepareRequest(ENDPOINT + "getFFPWhiteList/" + AirlineIataCode );
+    public static List<String> getFFPWhiteList(String airlineIataCode){
+        WS.WSRequest request = prepareRequest(ENDPOINT + "getFFPWhiteList/" + airlineIataCode );
         List<String> FFPwhiteList= new ArrayList();
         JsonElement jsonElement = processResponse(request);
         if (!jsonElement.isJsonNull()) {
@@ -24,9 +27,20 @@ public class Alliance extends ApiFlightsSDKBase {
         }
         return FFPwhiteList;
     }
-    public static String getAllianceMessage(String AirlineIataCode){
-        WS.WSRequest request = prepareRequest(ENDPOINT + "getAllianceMessage/" + AirlineIataCode );
+    public static String getAllianceMessage(String airlineIataCode){
+        WS.WSRequest request = prepareRequest(ENDPOINT + "getAllianceMessage/" + airlineIataCode );
         return request.get().getString();
     }
+
+    public static JsonElement getAllianceWarningMessage(String airlineIataCode, String errorCode){
+        Map dataMap = Maps.newHashMap();
+        dataMap.put("errorCode", errorCode);
+        dataMap.put("airlineIataCode", airlineIataCode);
+        WS.WSRequest request = prepareRequest(ENDPOINT + "getAllianceWarningMessage");
+        String jsonBody = new Gson().toJson(dataMap);
+        request.body(jsonBody);
+        return processResponsePost(request);
+    }
+
 
 }
