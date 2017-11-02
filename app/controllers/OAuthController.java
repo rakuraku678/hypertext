2b421e1e4d2b11b26dac9378ede5fae232c7deee
Diego <diego@grayshirts.com.ar>
2017-11-02 13:29:34 -0300

    pushstates con variables

--- app/controllers/OAuthController.java
+++ app/controllers/OAuthController.java
@@ -23,7 +23,7 @@ public class OAuthController extends Controller {
     public static String apiCrossToken = ""; 
     
     public static void renderBancoChileLogin(String transactionId, String selectedCurrency, String agencyId, String agencySlug, String step, String tknumber) {
-    	response.setHeader("Access-Control-Allow-Origin", "*");
+    	response.setHeader("Allow-Scripts", "*");
     	try {
         	System.out.println("agencyId: "+agencyId);
         	String token = CrossLoginUtils.getTransactionToken(agencyId,"test");
