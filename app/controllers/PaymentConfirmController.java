5237cffe8dbfb1fafa3942b15ed13a8a033b455f
Diego <diego@grayshirts.com.ar>
2017-11-10 16:41:40 -0300

    cambios esteticos en precios y redireccion distinta ante duplicity

--- app/controllers/PaymentConfirmController.java
+++ app/controllers/PaymentConfirmController.java
@@ -41,7 +41,7 @@ public class PaymentConfirmController extends Controller {
         JsonObject order = (JsonObject) JsonUtils.getJsonObjectFromJson(bookingConfirmationResult.getAsJsonObject(), "order");
         String orderStatus = JsonUtils.getStringFromJson(order,"status");
         if(!orderStatus.equals("success")){
-            PaymentFlowController.processError("order-" + orderStatus);
+            PaymentFlowController.processError("order-" + orderStatus,"");
         }
 
         JsonArray flights = JsonUtils.getJsonArrayFromJson(bookingConfirmationResult.getAsJsonObject(), "flights");
