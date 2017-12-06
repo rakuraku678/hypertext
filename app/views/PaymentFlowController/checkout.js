7f04fafb244e1113004db69c7567bb8ddc54f972
Diego <diego@grayshirts.com.ar>
2017-12-06 16:23:09 -0300

    adelantos

--- app/views/PaymentFlowController/checkout.js
+++ app/views/PaymentFlowController/checkout.js
@@ -317,6 +317,9 @@ function startBooking() {
     var formSerializeJson = $("#checkoutForm").serializeJSON({useIntKeysAsArrayIndex: true, checkboxUncheckedValue: false});
     formSerializeJson.bfmResultItem = $.parseJSON(formSerializeJson.bfmResultItem);
     formSerializeJson.origin = window.location.origin;
+    if (typeof finalSelectionArr !== 'undefined' && finalSelectionArr.length>0){
+    	formSerializeJson.seatsSelected =  finalSelectionArr.join();
+    }
     var data = JSON.stringify(formSerializeJson);
     $.ajax({
         type: "POST",
